27060c644b568287cd28ffbb349ddcadcbaf513e
Ömer Altıparmak <altiparmakomer@gmail.com>
2023-04-26 22:01:56 +0300

    v3

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -5,6 +5,8 @@ public class AtmProject {
         //version1
         System.out.println("java is easy");
         //version2
+        System.out.println("learn Java Earn Money");
+        //verson3
     }
 
 }
