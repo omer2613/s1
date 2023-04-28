603aabbf3a3dbc0de2887a5a9b29c21cc7be6510
Ömer Altıparmak <altiparmakomer@gmail.com>
2023-04-28 18:53:03 +0300

    ilk commit

--- src/Account.java
+++ src/Account.java
@@ -1,6 +1,7 @@
 public class Account {
     public static void main(String[] args) {
         System.out.println("bazi kodlar yazdim");
+        System.out.println("bazi kodlar yazdim");
 
     }
 }
